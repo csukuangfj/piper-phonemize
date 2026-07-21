d41c18fac7b8726c113d2f2e4445e5dd8138492e
Fangjun Kuang <csukuangfj@gmail.com>
2026-07-21 15:38:02 +0800

    Build static libs for android

--- build-android-armv7-eabi.sh
+++ build-android-armv7-eabi.sh
@@ -1,12 +1,30 @@
 #!/usr/bin/env bash
 set -ex
 
+if [ -z $BUILD_SHARED_LIBS ]; then
+  BUILD_SHARED_LIBS=ON
+fi
+
+if [ $BUILD_SHARED_LIBS == ON ]; then
+  dir=$PWD/build-android-armv7-eabi
+else
+  dir=$PWD/build-android-armv7-eabi-static
+fi
+
 if [ -z $ANDROID_NDK ]; then
   ANDROID_NDK=/star-fj/fangjun/software/android-sdk/ndk/22.1.7171670
+
+  if [ $BUILD_SHARED_LIBS == OFF ]; then
+    ANDROID_NDK=/star-fj/fangjun/software/android-sdk/ndk/27.0.11718014
+  fi
 
   if [ ! -d $ANDROID_NDK ]; then
     # For macOS with Android Studio
     ANDROID_NDK=/Users/fangjun/software/my-android/ndk/22.1.7171670
+
+    if [ $BUILD_SHARED_LIBS == OFF ]; then
+      ANDROID_NDK=/Users/fangjun/software/my-android/ndk/27.0.11718014
+    fi
   fi
 fi
 
@@ -16,8 +34,8 @@ if [ ! -d $ANDROID_NDK ]; then
 fi
 
 echo "ANDROID_NDK: $ANDROID_NDK"
+echo "BUILD_SHARED_LIBS: $BUILD_SHARED_LIBS"
 
-dir=$PWD/build-android-armv7-eabi
 mkdir -p $dir
 cd $dir
 
@@ -27,7 +45,7 @@ cmake \
   -DANDROID_PLATFORM=android-21 \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=./install \
-  -DBUILD_SHARED_LIBS=ON \
+  -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS \
   -DBUILD_PIPER_PHONEMIZE_PYTHON=OFF \
   -DBUILD_PIPER_PHONEMIZE_CORE_TESTS=OFF \
   ..
