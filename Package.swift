2f04fdd8d0951ee622a7336979f6c033c3ea13e4
Fangjun Kuang <csukuangfj@gmail.com>
2026-08-18 15:58:24 +0800

    Fix CI

--- Package.swift
+++ Package.swift
@@ -17,25 +17,29 @@ let package = Package(
     // --- Static binary targets ---
     .binaryTarget(
       name: "PiperPhonemizeMacOS",
-      url: "https://github.com/csukuangfj/piper-phonemize/releases/download/xcframework/piper-phonemize-v1.4.7-macos.xcframework.zip",
-      checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+      url:
+        "https://github.com/csukuangfj/piper-phonemize/releases/download/xcframework/piper-phonemize-v1.4.7-macos.xcframework.zip",
+      checksum: "7f96aec2316d8f5e04538ca1378d9d4ede7f9b48fe4cc8eba87e5d5868afbc01"
     ),
     .binaryTarget(
       name: "PiperPhonemizeIOS",
-      url: "https://github.com/csukuangfj/piper-phonemize/releases/download/xcframework/piper-phonemize-v1.4.7-ios.xcframework.zip",
-      checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+      url:
+        "https://github.com/csukuangfj/piper-phonemize/releases/download/xcframework/piper-phonemize-v1.4.7-ios.xcframework.zip",
+      checksum: "9db2d1eb865a752c48e84b94d3738f70b6d1372247d6b8637cc996df5dbdbd35"
     ),
 
     // --- Shared binary targets ---
     .binaryTarget(
       name: "PiperPhonemizeMacOSShared",
-      url: "https://github.com/csukuangfj/piper-phonemize/releases/download/xcframework/piper-phonemize-v1.4.7-macos-shared.xcframework.zip",
-      checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+      url:
+        "https://github.com/csukuangfj/piper-phonemize/releases/download/xcframework/piper-phonemize-v1.4.7-macos-shared.xcframework.zip",
+      checksum: "18d6132841eac41e3efb383b687547baf9d6ba5d45b0927dbd9f546b603043ec"
     ),
     .binaryTarget(
       name: "PiperPhonemizeIOSShared",
-      url: "https://github.com/csukuangfj/piper-phonemize/releases/download/xcframework/piper-phonemize-v1.4.7-ios-shared.xcframework.zip",
-      checksum: "0000000000000000000000000000000000000000000000000000000000000000"
+      url:
+        "https://github.com/csukuangfj/piper-phonemize/releases/download/xcframework/piper-phonemize-v1.4.7-ios-shared.xcframework.zip",
+      checksum: "0df4a24a76eedafa1fbddedd02f82d578a363049615cfe422160fc3fa1041549"
     ),
 
     // --- Static wrapper target (default) ---
