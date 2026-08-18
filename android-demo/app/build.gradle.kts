e1df5e427e99a0d61244f5742d70986c3e6e69ae
Fangjun Kuang <csukuangfj@gmail.com>
2026-08-18 19:38:13 +0800

    fix ci

--- android-demo/app/build.gradle.kts
+++ android-demo/app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.github.csukuangfj.piper-phonemize.piper.phonemize.demo"
+    namespace = "com.github.csukuangfj.piper.phonemize.demo"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.github.csukuangfj.piper-phonemize.piper.phonemize.demo"
+        applicationId = "com.github.csukuangfj.piper.phonemize.demo"
         minSdk = 26
         targetSdk = 34
         versionCode = 1
