3332048e201d6b24879c2ec5e9feaefb7e0b937d
Fangjun Kuang <csukuangfj@gmail.com>
2026-08-20 15:13:09 +0800

    Add Dart support

--- flutter/piper_phonemize/lib/src/piper_phonemize_flutter.dart
+++ flutter/piper_phonemize/lib/src/piper_phonemize_flutter.dart
@@ -5,7 +5,7 @@
 
 import 'dart:io';
 
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 
